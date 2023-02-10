5f588d43be2a5266e0c972c9286015785545305b
Daniel Mateu <danielmateu86@gmail.com>
2023-02-10 09:19:52 +0100

    Correccion en next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  nextConfig,
   env: {backendURL: 'http://localhost:4000'}
 }
+
+module.exports = nextConfig;
+
